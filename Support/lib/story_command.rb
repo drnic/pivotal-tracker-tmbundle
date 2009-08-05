9791c6a6bbcbadbda09f0a78a2a72bab7a250b67
Dr Nic Williams <drnicwilliams@gmail.com>
2009-08-05 11:06:19 -0500

    moved story snippet -> command

--- Support/lib/story_command.rb
+++ Support/lib/story_command.rb
@@ -1,3 +1,27 @@
 class StoryCommand
+  def render_snippet
+    default_snippet
+  end
   
+  def default_snippet
+    <<-EOS.gsub(/^    /, '')
+    name
+    	${1:Name of story}
+    description
+    	In order to ${2:...}
+    	As a ${3:role}
+    	I want ${4:feature}
+
+    	Acceptance:
+    	* ${10:do the thing
+    	* don't forget the other thing}
+    labels
+    	${20:comma,separated,labels}
+    ===============
+    EOS
+  end
+end
+
+if $0 == __FILE__
+  print StoryCommand.new.render_snippet
 end
--- Support/spec/story_command_spec.rb
+++ Support/spec/story_command_spec.rb
@@ -2,5 +2,28 @@ require File.dirname(__FILE__) + "/spec_helper"
 require 'story_command'
 
 describe StoryCommand do
+  describe "initial story in file" do
+    it "should return default story snippet" do
+      snippet = StoryCommand.new.render_snippet
+      snippet.should == <<-EOS.gsub(/^      /, '')
+      name
+      	${1:Name of story}
+      description
+      	In order to ${2:...}
+      	As a ${3:role}
+      	I want ${4:feature}
+
+      	Acceptance:
+      	* ${10:do the thing
+      	* don't forget the other thing}
+      labels
+      	${20:comma,separated,labels}
+      ===============
+      EOS
+    end
+  end
   
+  describe "next story in file" do
+    
+  end
 end
